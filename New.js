fdfc1c440d87d8dd4b18b809dbbbc544c2617ad1
suraj chauhan <sonubintw@gmail.com>
2022-12-21 12:02:16 +0530

    Create New.js
    this is all for practice and shit. #Javascript

---
+++ New.js
@@ -0,0 +1,63 @@
+// const student = [{ name: 'suraj', age: 20 },
+// { name: 'sonu', age: 21 }
+// ]
+
+
+// function pushingP(nameAge) {
+//     setTimeout(function () {
+//         student.push(nameAge);
+//         console.log('new Student has been pushed :)')
+//     }, 3000)
+// }
+
+
+// function displayP() {
+//     setTimeout(function () {
+//         console.log('this is the result ^-^')
+//         console.log(student)
+
+//     }, 1000)
+// }
+// pushingP({ name: "Bintw", age: 19 }, displayP);
+// console.log('hiiiiiiiiiiiii')
+
+// const number = [1, 2, 3, 4, 5]
+
+// function addNum(num, cb) {
+//     setTimeout(() => {
+//         number.push(num);
+//         console.log('Number entered by user has been pushed :)')
+//         cb()
+//     }, 5000)
+
+// }
+
+// function display() {
+//     setTimeout(() => {
+//         console.log('after update the numbers are');
+//         console.log(number);
+//     }, 1000)
+// }
+
+// addNum(1000, display);
+
+// const number = [1, 2, 3, 4, 5]
+
+
+// function unshiftt(num, callback) {
+//     number.unshift(num)
+//     console.log('number has been placed at front of the array ')
+//     callback()
+
+// }
+
+// function display() {
+//     console.log('after update the numbers are');
+//     console.log(number);
+// }
+
+
+// unshiftt(2001, display)
+
+
+
